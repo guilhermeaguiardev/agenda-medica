5e34fb34d7f42fdd53934f10dc9b68749364a867
Guilherme Aguiar <guilhermeaguiarsdev@gmail.com>
2026-04-16 23:16:39 -0300

    fix: porta dinâmica render

--- backend/server.js
+++ backend/server.js
@@ -100,6 +100,8 @@ app.post('/login', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando na porta', PORT);
 });
